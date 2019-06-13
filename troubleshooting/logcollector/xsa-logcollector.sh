ac34e7705f6a55b92d4c74b4e1a68df7cfcb3edf
Alessandro Stoltenberg <alessandro.stoltenberg@sap.com>
2019-06-13 15:41:46 +0200

    xsa: First version of xsa-logcollector script

--- troubleshooting/logcollector/xsa-logcollector.sh
+++ troubleshooting/logcollector/xsa-logcollector.sh
@@ -55,16 +55,23 @@ else
 	exit 0
 fi
 
-#Set the enviroment variables and restart the apps
+#Set the log-levels, enviroment variables and restart the apps
 echo -e "\nSetting log levels...\n"
-cf set-env "$approutername" XS_APP_LOG_LEVEL DEBUG
-cf set-env "$appname" SAP_EXT_TRC stdout
-cf set-env "$appname" SAP_EXT_TRL 3
-cf set-env "$appname" DEBUG xssec*
+xs set-logging-level "$approutername" "*" debug
+xs set-logging-level "$appname" "*" debug
+xs set-env "$approutername" REQUEST_TRACE true
+xs set-env "$appname" REQUEST_TRACE true
 
-echo -e "\nRestart the app and the approuter...\n"
-cf restart "$approutername"
-cf restart "$appname"
+#cf set-env "$approutername" XS_APP_LOG_LEVEL DEBUG
+#cf set-env "$appname" SAP_EXT_TRC stdout
+#cf set-env "$appname" SAP_EXT_TRL 3
+#cf set-env "$appname" DEBUG xssec*
+
+echo -e "\nRestage and restart the app and the approuter...\n"
+xs restage "$approutername"
+xs restart "$approutername"
+xs restage "$appname"
+xs restart "$appname"
 
 #Creating, collecting and compressing the logs
 echo -e "\n\e[36m\e[1mNow please repeat your scenario (e.g. try to login to your app or similar)...\e[0m\n"
@@ -74,16 +81,22 @@ echo -e "\nCollecting the logs..."
 
 { echo -e "Approuter logs:\n\n"; cf logs "$approutername" --recent; echo -e "\n\nApp logs:\n\n"; cf logs "$appname" --recent; } | zip -q "$logszip" -
 
-#Unsetting env variables and restarting apps
+#Unsetting log-levels, env variables and restarting apps
 echo -e "\nRestoring log levels...\n"
-cf unset-env "$approutername" XS_APP_LOG_LEVEL
-cf unset-env "$appname" SAP_EXT_TRC
-cf unset-env "$appname" SAP_EXT_TRLu 
-cf unset-env "$appname" DEBUG
+xs unset-logging-level "$approutername" "*"
+xs unset-logging-level "$appname" "*"
+xs unset-env "$approutername" REQUEST_TRACE
+xs unset-env "$appname" REQUEST_TRACE
+#cf unset-env "$approutername" XS_APP_LOG_LEVEL
+#cf unset-env "$appname" SAP_EXT_TRC
+#cf unset-env "$appname" SAP_EXT_TRLu 
+#cf unset-env "$appname" DEBUG
 
 echo -e "\nRestart the app and the approuter...\n"
-cf restart "$approutername"
-cf restart "$appname"
+xs restage "$approutername"
+xs restart "$approutername"
+xs restage "$appname"
+xs restart "$appname"
 
 #End
 echo -e "\n\e[32m\e[1mAll done.\e[0m Your file is here:" && readlink -f "$logszip"
