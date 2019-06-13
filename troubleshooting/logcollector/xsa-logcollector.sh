e9fedb45e11ffa8a12c6b76c5991139f91fa4d42
Alessandro Stoltenberg <alessandro.stoltenberg@sap.com>
2019-06-13 16:02:34 +0200

    xsa: use xs logs instead of cf logs

--- troubleshooting/logcollector/xsa-logcollector.sh
+++ troubleshooting/logcollector/xsa-logcollector.sh
@@ -79,7 +79,7 @@ read -rp "When you are done please press ENTER to collect the logs:"
 
 echo -e "\nCollecting the logs..."
 
-{ echo -e "Approuter logs:\n\n"; cf logs "$approutername" --recent; echo -e "\n\nApp logs:\n\n"; cf logs "$appname" --recent; } | zip -q "$logszip" -
+{ echo -e "Approuter logs:\n\n"; xs logs "$approutername" --recent; echo -e "\n\nApp logs:\n\n"; xs logs "$appname" --recent; } | zip -q "$logszip" -
 
 #Unsetting log-levels, env variables and restarting apps
 echo -e "\nRestoring log levels...\n"
